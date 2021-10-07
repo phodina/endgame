428745b169ccf395403d44f7f098024039047021
Petr Hodina <phodina@protonmail.com>
2021-10-07 20:31:02 +0200

    Add kaspersky-agent.

---
+++ guix-channel/endgame/packages/kaspersky.scm
@@ -0,0 +1,8 @@
+(define-module (endgame packages kaspersky)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (nonguix build-system binary)
+  #:use-module ((nonguix licenses) #:prefix license:))
+
+; TODO: Package deb file
+; https://www.kaspersky.com/small-to-medium-business-security/downloads/endpoint
