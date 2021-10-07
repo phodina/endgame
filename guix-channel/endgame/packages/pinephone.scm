94242d57a6ea30792bf23c3ebdfcd4801c131341
Petr Hodina <phodina@protonmail.com>
2021-10-07 21:37:48 +0200

    Add pinephone.

---
+++ guix-channel/endgame/packages/pinephone.scm
@@ -0,0 +1,6 @@
+; https://github.com/Biktorgj/quectel_eg25_recovery
+; https://git.spirirobotics.com/dchvs/eg25-g
+; https://codeberg.org/jackc/qptest
+; https://github.com/synacktiv/shannon-dbg
+; https://github.com/lololosys/awesome-baseband-research
+; https://github.com/grant-h/ShannonBaseband
