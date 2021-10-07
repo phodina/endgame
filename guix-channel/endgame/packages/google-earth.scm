42b40c477474940c4e91f2ea814401e0412e7d74
Petr Hodina <phodina@protonmail.com>
2021-10-07 22:17:29 +0200

    Add google-earth.

---
+++ guix-channel/endgame/packages/google-earth.scm
@@ -0,0 +1,8 @@
+(define-module (endgame packages google-earth)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (nonguix build-system binary)
+  #:use-module ((nonguix licenses) #:prefix license:))
+
+; TODO: Package deb file
+; https://www.addictivetips.com/ubuntu-linux-tips/google-earth-pro-on-linux/
