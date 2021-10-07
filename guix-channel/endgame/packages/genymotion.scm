45bc36e64be48872ff38c9bbd3dec1c4e5e942d8
Petr Hodina <phodina@protonmail.com>
2021-10-07 22:19:53 +0200

    Add genymotion.

---
+++ guix-channel/endgame/packages/genymotion.scm
@@ -0,0 +1,8 @@
+(define-module (endgame packages genymotion)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (nonguix build-system binary)
+  #:use-module ((nonguix licenses) #:prefix license:))
+
+; TODO: Package deb file
+; https://www.genymotion.com/download/
