e30acece92409327704d40f2b545c2f05336c03c
Petr Hodina <phodina@protonmail.com>
2021-10-07 22:21:44 +0200

    Add gnuhealth.

---
+++ guix-channel/endgame/packages/gnuhealth.scm
@@ -0,0 +1,7 @@
+(define-module (endgame packages gnuhealth)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (nonguix build-system binary)
+  #:use-module ((nonguix licenses) #:prefix license:))
+
+; https://en.wikibooks.org/wiki/GNU_Health/Installation
