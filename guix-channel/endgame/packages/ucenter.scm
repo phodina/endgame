3b36ec0a0d44aa973468e5d54effc5df87ac1d6f
Petr Hodina <phodina@protonmail.com>
2021-10-07 20:03:01 +0200

    Add ucenter.

---
+++ guix-channel/endgame/packages/ucenter.scm
@@ -0,0 +1,8 @@
+(define-module (endgame packages ucenter)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (nonguix build-system binary)
+  #:use-module ((nonguix licenses) #:prefix license:))
+
+; TODO: Attempt wine install
+; https://www.u-blox.com/en/product/u-center
